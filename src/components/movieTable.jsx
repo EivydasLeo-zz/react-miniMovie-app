a885a9839d16dd41960843c185a6a3f4dcf2839d
Your Name <you@example.com>
2021-06-11 09:16:32 +0300

    movieRow added

--- src/components/movieTable.jsx
+++ src/components/movieTable.jsx
@@ -1,31 +1,51 @@
 import React, { Component } from 'react';
 import { getMovies } from '../services/fakeMovieService';
-export default class MovieTable extends Component {
+import MovieRow from './movieRow';
+class MovieTable extends Component {
   state = {
     movies: getMovies(),
   };
 
   render() {
     const { movies: mv } = this.state;
-
     if (mv.length === 0) return <div className="alert alert-warning">There are no movies at the moment</div>;
 
     return (
       <div>
-        <h3>Pleasee see out movies</h3>
-        <p>Showing {mv.length} Movies in our store</p>
+        <h3>Please see out movies</h3>
+        <p>Showing {mv.length} movies in out store</p>
         <table className="table table-striped ">
           <thead>
             <tr>
               <th>Title</th>
               <th>Genre</th>
               <th>Stock</th>
-              <th>Rate</th>
+              <th>Rating</th>
               <th></th>
             </tr>
           </thead>
           <tbody>
-            {this.state.movies.map((m) => (
+            {this.state.movies.map((movie) => (
+              <MovieRow onDelete={this.handleDelete} movie={movie} key={movie._id} />
+            ))}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+  handleDelete = (movieId) => {
+    console.log('You are trying to delete, WHy ?', movieId);
+    const moviesWithoutTheOneWeDeleted = this.state.movies.filter((m) => m._id !== movieId);
+    this.setState({ movies: moviesWithoutTheOneWeDeleted });
+  };
+}
+
+export default MovieTable;
+
+/**
+ * 
+ * 
+ * {this.state.movies.map((m) => (
               <tr key={m._id}>
                 <th>{m.title}</th>
                 <td>{m.genre.name}</td>
@@ -38,14 +58,6 @@ export default class MovieTable extends Component {
                 </td>
               </tr>
             ))}
-          </tbody>
-        </table>
-      </div>
-    );
-  }
-  handleDelete = (id) => {
-    console.log('u want to delete me?', id);
-    const movies = this.state.movies.filter((m) => m._id !== id);
-    this.setState({ movies });
-  };
-}
+ * 
+ * 
+ */
