d94033d294aa75f026a576e76f759a6cf896e5a7
Your Name <you@example.com>
2021-06-11 11:13:45 +0300

    getting list of todos

--- src/components/appHeader/appHeader.jsx
+++ src/components/appHeader/appHeader.jsx
@@ -4,14 +4,7 @@ import './style.css';
 // add image
 import natureImg from './nature.jpg';
 class AppHeader extends Component {
-  state = {
-    todos: [
-      { id: 1, isDone: false, title: 'Buy Milk' },
-      { id: 2, isDone: true, title: 'Buy Tv' },
-      { id: 3, isDone: false, title: 'Go to park' },
-      { id: 4, isDone: false, title: 'Learn to React' },
-    ],
-  };
+  state = {};
 
   getDate() {
     const now = new Date().toLocaleString();
