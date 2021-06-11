47159059228d7cfe9caef1b7cf1eb336e8cd1bb7
Your Name <you@example.com>
2021-06-11 10:52:20 +0300

    get current time done

--- src/components/appHeader/appHeader.jsx
+++ src/components/appHeader/appHeader.jsx
@@ -4,11 +4,25 @@ import './style.css';
 // add image
 import natureImg from './nature.jpg';
 class AppHeader extends Component {
-  state = {};
+  state = {
+    todos: [
+      { id: 1, isDone: false, title: 'Buy Milk' },
+      { id: 2, isDone: true, title: 'Buy Tv' },
+      { id: 3, isDone: false, title: 'Go to park' },
+      { id: 4, isDone: false, title: 'Learn to React' },
+    ],
+  };
+
+  getDate() {
+    const now = new Date().toLocaleString();
+    return now;
+  }
+
   render() {
     return (
       <header>
         <img className="nature" src={natureImg} alt="Very nice view of nature with house" />
+        <p className="hero-date">{this.getDate()}</p>
       </header>
     );
   }
