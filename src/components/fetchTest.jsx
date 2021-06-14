df7986081e31760f518ea8e0c96fefc99c2572fa
Your Name <you@example.com>
2021-06-14 11:48:14 +0300

    new todo btn

--- src/components/fetchTest.jsx
+++ src/components/fetchTest.jsx
@@ -9,11 +9,16 @@ class FetchTest extends Component {
     this.setState({ todoTitle: e.target.value });
   };
 
+  handleNewTodo = () => {
+    console.log('works like a charm');
+  };
+
   render() {
     return (
       <div>
         <h1>Fetch test</h1>
         <input onChange={this.syncTitle} value={this.state.todoTitle} type="text" placeholder="add new todo" />
+        <button onClick={this.handleNewTodo}>Save New Todo</button>
       </div>
     );
   }
