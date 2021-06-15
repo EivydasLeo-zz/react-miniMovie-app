b47e005580a5e84e03945938290691248a52936c
Your Name <you@example.com>
2021-06-15 10:24:48 +0300

    install router setup index.js

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,9 +1,30 @@
 import React, { Component } from 'react';
-
+// importuojam modulini individualu css
+import navStyles from './navbar.module.css';
 class Navbar extends Component {
   state = {};
   render() {
-    return <h2>Navbar</h2>;
+    return (
+      <nav className={navStyles.navbar}>
+        <a className="nav-link" href="/">
+          ReactApp
+        </a>
+        <div className="nav-links">
+          <a className="nav-link" href="/">
+            Home
+          </a>
+          <a className="nav-link" href="/todos">
+            Todos
+          </a>
+          <a className="nav-link" href="/about">
+            About
+          </a>
+          <a className="nav-link" href="/contact">
+            Contact
+          </a>
+        </div>
+      </nav>
+    );
   }
 }
 
